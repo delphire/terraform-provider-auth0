658a01e0896d99e51fb178bf9001c43e3cd01327
Christian Bradley <christian.bradley@bocodigital.com>
2017-06-04 17:04:00 -0400

    update resource rule

--- resource_rule.go
+++ resource_rule.go
@@ -8,16 +8,17 @@ import (
 
 func resourceRule() *schema.Resource {
 	return &schema.Resource{
-		Exists: resourceRuleExists,
-		Create: resourceRuleCreate,
-		Read:   resourceRuleRead,
-		Update: resourceRuleUpdate,
-		Delete: resourceRuleDelete,
-		Schema: resourceSchemaMap(),
+		Exists:   resourceRuleExists,
+		Create:   resourceRuleCreate,
+		Read:     resourceRuleRead,
+		Update:   resourceRuleUpdate,
+		Delete:   resourceRuleDelete,
+		Importer: &schema.ResourceImporter{},
+		Schema:   resourceRuleSchema(),
 	}
 }
 
-func resourceSchemaMap() map[string]*schema.Schema {
+func resourceRuleSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
 		"name":    schemaString(),
 		"enabled": schemaBool(),
@@ -30,7 +31,7 @@ func resourceRuleExists(d *schema.ResourceData, meta interface{}) (bool, error) 
 	svc := metaRuleService(meta)
 	_, _, err := svc.Read(d.Id(), nil)
 
-	if _, ok := err.(*auth0mgmt.ApiError); ok {
+	if resourceRuleIsApi404Err(err) {
 		return false, nil
 	}
 
@@ -39,7 +40,7 @@ func resourceRuleExists(d *schema.ResourceData, meta interface{}) (bool, error) 
 
 func resourceRuleCreate(d *schema.ResourceData, meta interface{}) error {
 	svc := metaRuleService(meta)
-	params := getRuleProperties(d)
+	params := resourceRuleGetProperties(d)
 	r, _, err := svc.Create(params)
 
 	if err != nil {
@@ -59,31 +60,36 @@ func resourceRuleRead(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 
-	setRuleData(r, d)
+	resourceRuleSetData(r, d)
 
 	return nil
 }
 
 func resourceRuleUpdate(d *schema.ResourceData, meta interface{}) error {
 	svc := metaRuleService(meta)
-	params := getRuleProperties(d)
+	params := resourceRuleGetProperties(d)
 	r, _, err := svc.Update(d.Id(), params)
 
 	if err != nil {
 		return err
 	}
 
-	setRuleData(r, d)
+	resourceRuleSetData(r, d)
 	return nil
 }
 
 func resourceRuleDelete(d *schema.ResourceData, meta interface{}) error {
 	svc := metaRuleService(meta)
 	_, err := svc.Delete(d.Id())
-	return err
+
+	if resourceRuleIsApi404Err(err) {
+		return nil
+	} else {
+		return err
+	}
 }
 
-func getRuleProperties(d *schema.ResourceData) *auth0rule.Properties {
+func resourceRuleGetProperties(d *schema.ResourceData) *auth0rule.Properties {
 	return &auth0rule.Properties{
 		Name:    d.Get("name").(string),
 		Script:  d.Get("script").(string),
@@ -92,10 +98,15 @@ func getRuleProperties(d *schema.ResourceData) *auth0rule.Properties {
 	}
 }
 
-func setRuleData(p *auth0rule.Rule, d *schema.ResourceData) {
-	d.SetId(p.Id.(string))
+func resourceRuleSetData(p *auth0rule.Rule, d *schema.ResourceData) {
+	// d.SetId(p.Id.(string))
 	d.Set("name", p.Name.(string))
 	d.Set("script", p.Script.(string))
 	d.Set("enabled", p.Enabled.(bool))
 	d.Set("order", p.Order)
+}
+
+func resourceRuleIsApi404Err(err error) bool {
+	apiErr, ok := err.(*auth0mgmt.ApiError)
+	return ok && apiErr.StatusCode == 404
 }
